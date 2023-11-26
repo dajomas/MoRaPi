d6bce1d5d4ebd78b8d1343977fdda7e6a303e582
Dajomas <dajomas@gmail.com>
2023-11-26 10:24:09 +0100

    Simplify checking values and debugging

--- run.py
+++ run.py
@@ -23,14 +23,38 @@ class run_train(object):
             retval = False
         return [retval,oldval]
 
-    def verify_program(self, cmd_line_options, debug):
+    def check_value(self, cmd_list, cmd_line, value, type):
+        __type = str(type).upper()
+        Err = ''
+        if self.__debug > 1:
+            print("Checking value for cmd_line ("+str(cmd_line)+") ("+type+"): ")
+            print(value)
+        if value[0] in [errIndexError,errNoValue]:
+            Err = ': value for '+__type+': is missing'
+        else:
+            if value[0] == errValueError:
+                Err = ': invalid value for '+__type+': '+str(value[1])
+            elif __type == 'SPEED':
+                if (round(value[0],0) != okIgnore and not(round(value[0],3) >= 0 and round(value[0],3) <= 1)):
+                    Err = ': invalid value for '+__type+': '+str(value[0])
+            elif __type == 'DIRECTION':
+                if (value[0] != okIgnore and value[0] != -1 and value[0] != 0 and value[0] != 1):
+                    Err = ': invalid value for '+__type+': '+str(value[0])
+            else:
+                if (value[0] != okIgnore and value[0] < 0):
+                    Err = ': invalid value for '+__type+': '+str(value[0])
+        if Err != '':
+            cmd_list.append(str(cmd_line)+str(Err))
+        return cmd_list
+
+    def verify_program(self, cmd_line_options):
         cmd_list = {'ERRORS': [], 'CMDS': []}
         cmd_line = 0
         for cmd in cmd_line_options:
             cmd_line += 1
             if len(cmd) == 0:
                 continue
-            if debug > 1:
+            if self.__debug > 1:
                 print("Processing command:")
                 print(cmd)
             if cmd[0] in cmd_list['CMDS']:
@@ -70,52 +94,21 @@ class run_train(object):
                 elif cmd[1] not in ['stop']:
                     cmd_list['ERRORS'].append(str(cmd_line)+': Unknwon command '+str(cmd[1])+'.')
 
-                if __track[0] in [errIndexError,errNoValue] :
-                    cmd_list['ERRORS'].append(str(cmd_line)+': value for TRACK is missing')
-                elif __track[0] == errValueError or (__track[0] != okIgnore and __track[0] < 0):
-                    cmd_list['ERRORS'].append(str(cmd_line)+': invalid value for TRACK.'+str(__track[1]))
-
-                if __speed[0] in [errIndexError,errNoValue] :
-                    cmd_list['ERRORS'].append(str(cmd_line)+': value for SPEED is missing')
-                elif __speed[0] == errValueError or (round(__speed[0],0) != okIgnore and not(round(__speed[0],3) >= 0 and round(__speed[0],3) <= 1)):
-                    cmd_list['ERRORS'].append(str(cmd_line)+': invalid value for SPEED.'+str(__speed[1]))
-
-                if __direction[0] in [errIndexError,errNoValue]:
-                    cmd_list['ERRORS'].append(str(cmd_line)+': value for DIRECTION is missing')
-                elif __direction[0] == errValueError or (__direction[0] != okIgnore and __direction[0] != -1 and __direction[0] != 0 and __direction[0] != 1):
-                    cmd_list['ERRORS'].append(str(cmd_line)+': invalid value for DIRECTION '+str(__direction[1]))
-
-                if __duration[0] in [errIndexError,errNoValue]:
-                    cmd_list['ERRORS'].append(str(cmd_line)+': value for DURATION is missing')
-                elif __duration[0] == errValueError or (__duration[0] != okIgnore and __duration[0] < 0):
-                    cmd_list['ERRORS'].append(str(cmd_line)+': invalid value for DURATION '+str(__duration[1]))
-
-                if __switch[0]in [errIndexError,errNoValue]:
-                    cmd_list['ERRORS'].append(str(cmd_line)+': value for SWITCH is missing')
-                elif __switch[0] == errValueError or (__switch[0] != okIgnore and __switch[0] < 0):
-                    cmd_list['ERRORS'].append(str(cmd_line)+': invalid value for SWITCH '+str(__switch[1]))
-
-                if __count[0] in [errIndexError,errNoValue]:
-                    cmd_list['ERRORS'].append(str(cmd_line)+': value for COUNT is missing')
-                elif __count[0] == errValueError or (__count[0] != okIgnore and __count[0] < 0):
-                    cmd_list['ERRORS'].append(str(cmd_line)+': invalid value for COUNT '+str(__count[1]))
-
-                if __point[0]in [errIndexError,errNoValue]:
-                    cmd_list['ERRORS'].append(str(cmd_line)+': value for POINT is missing')
-                elif __point[0] == errValueError or (__point[0] != okIgnore and __point[0] < 0):
-                    cmd_list['ERRORS'].append(str(cmd_line)+': invalid value for POINT '+str(__switch[1]))
-
-                if __force[0]in [errIndexError,errNoValue]:
-                    cmd_list['ERRORS'].append(str(cmd_line)+': value for FORCE is missing')
-                elif __force[0] == errValueError or (__force[0] != okIgnore and __force[0] < 0):
-                    cmd_list['ERRORS'].append(str(cmd_line)+': invalid value for FORCE '+str(__switch[1]))
+                cmd_list['ERRORS'] = self.check_value(cmd_list['ERRORS'],cmd_line,__track,'TRACK')
+                cmd_list['ERRORS'] = self.check_value(cmd_list['ERRORS'],cmd_line,__speed,'SPEED')
+                cmd_list['ERRORS'] = self.check_value(cmd_list['ERRORS'],cmd_line,__direction,'DIRECTION')
+                cmd_list['ERRORS'] = self.check_value(cmd_list['ERRORS'],cmd_line,__duration,'DURATION')
+                cmd_list['ERRORS'] = self.check_value(cmd_list['ERRORS'],cmd_line,__sensor,'SENSOR')
+                cmd_list['ERRORS'] = self.check_value(cmd_list['ERRORS'],cmd_line,__count,'COUNT')
+                cmd_list['ERRORS'] = self.check_value(cmd_list['ERRORS'],cmd_line,__point,'POINT')
+                cmd_list['ERRORS'] = self.check_value(cmd_list['ERRORS'],cmd_line,__force,'FORCE')
 
         return cmd_list['ERRORS']
 
-    def run_program(self, t, cmd_line_options, debug):
+    def run_program(self, t, cmd_line_options):
         cmd_line = 0
         for cmd in sorted(cmd_line_options, key=lambda x:x[0]):
-            if debug > 0:
+            if self.__debug > 0:
                 print("Executing command: "+" ".join(str(x) for x in cmd))
             cmd_line += 1
             if cmd[1] == 'set_track':
@@ -158,14 +151,15 @@ class run_train(object):
         return True
 
     def process_commands(self, t, cmd_options, debug):
-        error_list = self.verify_program(cmd_options, debug)
+        self.__debug = debug
+        error_list = self.verify_program(cmd_options)
         if len(error_list) > 0:
             print("Errors found in commands:")
             for Err in error_list:
                 print(Err)
             return False
         else:
-            return self.run_program(t, cmd_options, debug)
+            return self.run_program(t, cmd_options)
 
 if __name__ == '__main__':
     print("This Python script can not be used standalone.")
