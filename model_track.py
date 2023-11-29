9165cdec2f17de50e14b2dad468584fbe7b9c723
Dajomas <dajomas@gmail.com>
2023-11-29 22:15:35 +0100

    Allow model_track.yml file to override default values

--- model_track.py
+++ model_track.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import argparse
 import textwrap
+import yaml
 
 from run import run_track
 from itrack import iTrack
@@ -22,34 +23,34 @@ def demo0(e):
     e.run_for(0.5,e.go_backward,5)
     e.stop()
 
-def process_args():
+def process_args(dargs):
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                      epilog=textwrap.dedent(help_text))
 
-    parser.add_argument('-v','--v','--verbose', dest='debug', action='count', default=0, help="increase verbosity, more v's is more output (default: 0)")
-    parser.add_argument('--name', dest='name', type=str, action='store', default='track', help='Name of the track (default: track)')
+    parser.add_argument('-v','--v','--verbose', dest='debug', action='count', default=dargs['debug'], help="increase verbosity, more v's is more output (default: 0)")
+    parser.add_argument('--name', dest='name', type=str, action='store', default=dargs['name'], help='Name of the track (default: track)')
 
-    parser.add_argument('-f','--f','--function', choices=['set_speed','run_for','run_until','demo','program','script','interactive'], type=str, dest='function', default='interactive', help='Function to run (default: interactive)')
+    parser.add_argument('-f','--f','--function', choices=['set_speed','run_for','run_until','demo','program','script','interactive'], type=str, dest='function', default=dargs['function'], help='Function to run (default: '+str(dargs['function'])+')')
 
-    parser.add_argument('--host', dest='host', type=str, action='store', default='localhost', help='Name of the host that runs pigpiod (default: localhost)')
-    parser.add_argument('--port', dest='port', type=int, action='store', default=8888, help='Port on which pigpio is listening (default: 8888)')
+    parser.add_argument('--host', dest='host', type=str, action='store', default=dargs['host'], help='Name of the host that runs pigpiod (default: '+str(dargs['host'])+')')
+    parser.add_argument('--port', dest='port', type=int, action='store', default=dargs['port'], help='Port on which pigpio is listening (default: '+str(dargs['port'])+')')
 
-    parser.add_argument('--steps', dest='steps', type=int, action='store', default=10, help='Number of steps to go from MIN/MAX to MAX/MIN (default: 10)')
-    parser.add_argument('--ctime', dest='ctime', type=int, action='store', default=5, help='Number of seconds to go from MIN/MAX to MAX/MIN (default: 5)')
+    parser.add_argument('--steps', dest='steps', type=int, action='store', default=dargs['steps'], help='Number of steps to go from MIN/MAX to MAX/MIN (default: '+str(dargs['steps'])+')')
+    parser.add_argument('--ctime', dest='ctime', type=int, action='store', default=dargs['ctime'], help='Number of seconds to go from MIN/MAX to MAX/MIN (default: '+str(dargs['ctime'])+')')
 
-    parser.add_argument('--list-presets', dest='list_presets', default=False, action='store_true',help='List all available presets (use the preset number with --preset)')
-    parser.add_argument('--preset', dest='presets', action='append', type=int, default=[], help='use predefined presets to configure a track segment (can be used multiple times)')
-    parser.add_argument('--fwd', dest='pin_fwd', type=int, action='store', default=17, help='GPIO number of the forward pin (default: 17) (ignored if --track is used)')
-    parser.add_argument('--rev', dest='pin_rev', type=int, action='store', default=18, help='GPIO number of the backward pin (default: 18) (ignored if --track is used)')
-    parser.add_argument('--enable', dest='pin_enable', type=int, action='store', default=None, help='GPIO number of the enable pin (default: None) (ignored if --track is used)')
-    parser.add_argument('--track', nargs='*', type=int, action='append', dest='tracks', default=[], help='Define track as <fwd-pin> <rev-pin> [<enable-pin>]')
-    parser.add_argument('-s','--s','--sensor', dest='sensor_pins', type=int, action='append', default=[], help='GPIO number of a sensor, can be specified multiple times, for run_until uses the first sensor, so at least one is required (default: [])')
-    parser.add_argument('-p','--p','--point', dest='point_pins', type=int, action='append', default=[], help='GPIO number of a point, can be specified multiple times (default: [])')
+    parser.add_argument('--list-presets', dest='list_presets', default=dargs['list_presets'], action='store_true',help='List all available presets (use the preset number with --preset)')
+    parser.add_argument('--preset', dest='presets', action='append', type=int, default=dargs['presets'], help='use predefined presets to configure a track segment (can be used multiple times) (default: '+str(dargs['presets'])+')')
+    parser.add_argument('--fwd', dest='pin_fwd', type=int, action='store', default=dargs['pin_fwd'], help='GPIO number of the forward pin (default: '+str(dargs['pin_fwd'])+') (ignored if --track or --preset is used)')
+    parser.add_argument('--rev', dest='pin_rev', type=int, action='store', default=dargs['pin_rev'], help='GPIO number of the backward pin (default: '+str(dargs['pin_rev'])+') (ignored if --track or --preset is used)')
+    parser.add_argument('--enable', dest='pin_enable', type=int, action='store', default=dargs['pin_enable'], help='GPIO number of the enable pin (default: '+str(dargs['pin_enable'])+') (ignored if --track or --preset is used)')
+    parser.add_argument('--track', nargs='*', type=int, action='append', dest='tracks', default=dargs['tracks'], help='Define track as <fwd-pin> <rev-pin> [<enable-pin>]')
+    parser.add_argument('-s','--s','--sensor', dest='sensor_pins', type=int, action='append', default=dargs['sensor_pins'], help='GPIO number of a sensor, can be specified multiple times, for run_until uses the first sensor, so at least one is required (default: '+str(dargs['sensor_pins'])+')')
+    parser.add_argument('-p','--p','--point', dest='point_pins', type=int, action='append', default=dargs['point_pins'], help='GPIO number of a point, can be specified multiple times (default: '+str(dargs['point_pins'])+')')
 
-    parser.add_argument('--speed', dest='speed', type=float, action='store', default=1, help='for set_speed, run_for and run_until (default: 1)')
-    parser.add_argument('--direction', dest='direction', type=str, choices=valid_direction, action='store', default=1, help='-1 is backwards, 1 is forward, for set_speed, run_for and run_until (default: 1)')
-    parser.add_argument('--duration', dest='duration', type=int, action='store', default=10, help='duration in seconds, for run_for only (default: 10)')
-    parser.add_argument('--count', dest='count', type=int, action='store', default=1, help='Number of time the sensor should be passed before triggering, for run_until only (default: 1)')
+    parser.add_argument('--speed', dest='speed', type=float, action='store', default=dargs['speed'], help='for set_speed, run_for and run_until (default: '+str(dargs['speed'])+')')
+    parser.add_argument('--direction', dest='direction', type=str, choices=valid_direction, action='store', default=dargs['direction'], help='-1 is backwards, 1 is forward, for set_speed, run_for and run_until (default: '+str(dargs['direction'])+')')
+    parser.add_argument('--duration', dest='duration', type=int, action='store', default=dargs['duration'], help='duration in seconds, for run_for only (default: '+str(dargs['duration'])+')')
+    parser.add_argument('--count', dest='count', type=int, action='store', default=dargs['count'], help='Number of time the sensor should be passed before triggering, for run_until only (default: '+str(dargs['count'])+')')
 
     parser.add_argument('--script', dest='script', type=argparse.FileType('r'), help='scriptfile containing a command per line. (See explanation below)')
     parser.add_argument('--program', nargs='*', action='append', dest='cmd_options', help='run set_speed, run_for or run_until with options. (See explanation below)')
@@ -57,8 +58,20 @@ def process_args():
 
     return parser.parse_args()
 
+def read_config_file():
+    try:
+        with open('model_track.yml', 'r') as config_file:
+            config_dict = yaml.safe_load(config_file)
+            config_file.close()
+        new_config = default_args
+        for key in config_dict['model_track'].keys():
+            new_config[key] = config_dict['model_track'][key]
+        return new_config
+    except:
+        return default_args
+
 def main():
-    args = process_args()
+    args = process_args(read_config_file())
 
     DEBUG = args.debug
 
@@ -104,7 +117,7 @@ def main():
         t.run_for(args.speed, args.direction, args.duration)
     elif args.function == 'run_until':
         if len(args.sensor_pins) == 0:
-            print('At least one sensor should be define if you use the run_until function')
+            print('At least one sensor should be defined if you use the run_until function')
             args.print_help()
         else:
             if DEBUG > 0:
