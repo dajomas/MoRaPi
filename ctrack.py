47feef3851b355e92a955dfe865ca798fa19a428
Dajomas <dajomas@gmail.com>
2023-12-14 10:25:49 +0100

    Add sensors to stats display and resize windows

--- ctrack.py
+++ ctrack.py
@@ -1,7 +1,8 @@
 import curses
 import string
 import threading
-from time import sleep
+from time import time,sleep
+from datetime import timedelta
 
 from defaults import *
 from run import run_track
@@ -38,9 +39,24 @@ class cTrack(object):
         self.__cmds_win = self.__mk_win(1,1,1,1)
         self.__outp_win = self.__mk_win(1,1,1,1)
 
-        self.__do_resize()
-
         self.__msgs = []
+        self.__sensor_trigger = {}
+        self.__sensor_trigger_delta = {}
+        self.__sensor_trigger_count = {}
+
+        self.__cmd_help = [
+            "1-9,0: set speed (speed up) for current track 1=10%, 0=100%",
+            "r:     set di,ection and speed for current track",
+            "d:     toggle direction for current track (slow down/speed u,)",
+            "s:     stop (slow down) current track",
+            "e:     emergency stop current track",
+            "t:     switch active track",
+            "p:     toggle point",
+            "i:     display status info",
+            "x/q:   Exit"
+        ]
+
+        self.__do_resize()
 
     def __mk_win(self,h,w,y,x):
         win = curses.newwin(h,w,y,x)
@@ -66,9 +82,9 @@ class cTrack(object):
         y, x = self.__screen.getmaxyx()
         w = []
         extra = 0 if int(y/2)*2 == y else 1
-        w.append({'x': int((x/3)*2)-2, 'y': int((y/2)-2+extra)})
-        w.append({'x': int((x/3)*2)-2, 'y': int((y/2)-2)})
-        w.append({'x': x-(int((x/3)*2)-2)-4, 'y': y-3})
+        w.append({'x': 70, 'y': int(y-len(self.__cmd_help)-7-2+extra)})
+        w.append({'x': 70, 'y': int(len(self.__cmd_help)+6)})
+        w.append({'x': x-70-2-4, 'y': y-2})
         self.__screen.clear
         self.__screen.refresh()
 
@@ -105,23 +121,51 @@ class cTrack(object):
             lcnt = 0
             for one_track in self.__track.tracks:
                 speed_graph="*"*(int(one_track['speed']*10)-1)
+                isactive = "*" if lcnt == self.__track.active_track else " "
                 if one_track['direction'] == -1:
                     speed_graph = str("<"+speed_graph).rjust(10)+"|"+str(" "*10)
                 elif one_track['direction'] == 1:
                     speed_graph = str(" "*10)+"|"+str(speed_graph+">").ljust(10)
                 else:
                     speed_graph = str(" "*10)+"|"+str(" "*10)
-                self.__stat_win.addstr(lcnt+4,2, str(lcnt).rjust(3).ljust(4)+str(str(one_track['speed']*100)+"%").rjust(8).ljust(10)+one_track['direction_str'].ljust(10)+"  ")
+                self.__stat_win.addstr(lcnt+4,2,isactive+str(lcnt).rjust(2).ljust(4)+str(str(one_track['speed']*100)+"%").rjust(8).ljust(10)+one_track['direction_str'].ljust(10)+"  ")
                 self.__stat_win.addstr(lcnt+4,29,speed_graph[:10],curses.color_pair(1))
                 self.__stat_win.addstr(lcnt+4,39,speed_graph[10],curses.color_pair(2))
                 self.__stat_win.addstr(lcnt+4,40,speed_graph[11:],curses.color_pair(3))
                 lcnt += 1
+            if len(self.__track.sensors) > 0:
+                self.__stat_win.addstr(lcnt+6,2,"Sensor  Last triggered  Last lap time  Laps")
+                self.__stat_win.addstr(lcnt+7,2,"------  --------------  -------------  ----")
+                scnt = lcnt+8
+                for one_sensor in self.__track.sensors:
+                    pin = str(one_sensor)
+                    self.__stat_win.addstr(scnt, 2, "GPIO"+pin.rjust(2,'0'))
+                    if pin in self.__sensor_trigger.keys():
+                        secs = int(time()) - self.__sensor_trigger[pin]
+                        if secs < 60:
+                            ago = str(secs)+" sec ago"
+                        else:
+                            ago = str(int(secs/60))+" min ago"
+                    else:
+                        ago = "---"
+                    self.__stat_win.addstr(scnt, 10, ago.ljust(14))
+                    if pin in self.__sensor_trigger_delta.keys():
+                        delta = timedelta(seconds=self.__sensor_trigger_delta[pin])
+                    else:
+                        delta = "---"
+                    self.__stat_win.addstr(scnt, 26, str(delta).ljust(8))
+                    if pin in self.__sensor_trigger_count.keys():
+                        laps = str(self.__sensor_trigger_count[pin])
+                    else:
+                        laps = "0"
+                    self.__stat_win.addstr(scnt, 41, str(laps).rjust(4))
+                    scnt += 1
             self.__paint_wins()
             sleep(1)
 
     def __wait_for_key(self, text):
         y,x = self.__cmds_win.getmaxyx()
-        self.__cmds_win.addstr(y-2,1, text)
+        self.__cmds_win.addstr(y-3,2, text)
         self.__cmds_win.clrtoeol()
         self.__paint_wins()
         return self.__cmds_win.getkey(y-2,len(text)+2)
@@ -135,6 +179,13 @@ class cTrack(object):
 
     def __sensor_callback(self,press):
         pin = int(str(press.pin)[4:])
+        if str(pin) in  self.__sensor_trigger.keys():
+            self.__sensor_trigger_delta[str(pin)] = int(time())-self.__sensor_trigger[str(pin)]
+        if str(pin) in  self.__sensor_trigger_count.keys():
+            self.__sensor_trigger_count[str(pin)] +=1
+        else:
+            self.__sensor_trigger_count[str(pin)] = 1
+        self.__sensor_trigger[str(pin)] = int(time())
         self.__writelog('* Passing sensor: '+str(self.__track.sensor_id(pin=pin))+' ('+str(press.pin)+')')
 
     # Set speed
@@ -171,14 +222,9 @@ class cTrack(object):
         display_stats.start()
 
         while self.__do_run:
-            self.__cmds_win.addstr(2,2, "1-9,0: set speed for current track 1=10%, 0=100%")
-            self.__cmds_win.addstr(3,2, "r:     set direction and speed for current track")
-            self.__cmds_win.addstr(4,2, "d:     toggle direction for current track")
-            self.__cmds_win.addstr(5,2, "s:     stop current track")
-            self.__cmds_win.addstr(6,2, "t:     switch track")
-            self.__cmds_win.addstr(7,2, "p:     toggle point")
-            self.__cmds_win.addstr(8,2, "i:     display status info")
-            self.__cmds_win.addstr(9,2, "x/q:   Exit")
+            linenr = 1
+            for linetxt in self.__cmd_help:
+                self.__cmds_win.addstr((linenr:=linenr+1),2, linetxt)
             self.__paint_wins()
 
             c = self.__wait_for_key("Command:").lower()
@@ -186,7 +232,7 @@ class cTrack(object):
             if c == 410 or c == "key_resize":
                 self.__do_resize()
             elif c in set(string.ascii_lowercase + string.digits):
-                if c in ['x','q']:
+                if c in ['x','q'] or ord(c) == 27:
                     self.__do_run = False
                 elif c in ['0','1','2','3','4','5','6','7','8','9']:
                     speed = 1 if c == '0' else int(c)/10
